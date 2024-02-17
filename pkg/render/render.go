86484644d4317312be3d9778dbe9e280b39d7a8f
NitishKumar06 <justnitish06@gmail.com>
2024-02-17 17:23:07 +0530

    render template using cache
    Signed-off-by: NitishKumar06 <justnitish06@gmail.com>

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,21 +1,72 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
+// RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-
-	parsedTemplate, err := template.ParseFiles("./template/"+tmpl, "./template/base.layout.gohtml")
+	// create a template cache
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
-	if err := parsedTemplate.Execute(w, nil); err != nil {
-		fmt.Println(err)
-		return
+	// get template from the template cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
 	}
+
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// render the fetched template
+	_, err = //fmt.Println(files)
+		buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	// get all files that ends with .page.gohtml
+	files, err := filepath.Glob("./templates/*.page.gohtml")
+	if err != nil {
+		return cache, err
+	}
+
+	for _, file := range files {
+		name := filepath.Base(file)
+		tmplSet, err := template.New(name).ParseFiles(file)
+		if err != nil {
+			return cache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.gohtml")
+		if err != nil {
+			return cache, err
+		}
+
+		if len(matches) > 0 {
+			tmplSet, err = tmplSet.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				return cache, err
+			}
+		}
+
+		cache[name] = tmplSet
+	}
+
+	return cache, nil
 }
